d4bddcff611c79303e68b8ee7d82efaa7f86156c
Dovciak, Adam <a.dovciak@liverpool.ac.uk>
2024-07-18 19:54:38 +0100

    Initial Commit

---
+++ DeveloperExtraTool/DeveloperExtraToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeveloperExtraToolApp.swift
+//  DeveloperExtraTool
+//
+//  Created by Adam Dovciak on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct DeveloperExtraToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
